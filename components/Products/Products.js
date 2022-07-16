25a3833540eb30c64652c89db63baa5e60384ff4
Volodimir <dev.v.nazarenko@gmail.com>
2022-07-16 15:01:54 +0300

    Bound local storage class with products

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,11 +1,49 @@
 class Products
 {
+
+  constructor()
+  {
+    this.classNameActive = 'products-element-btn-active';
+    this.labelAdd = 'Add to cart';
+    this.labelRemove = 'Delete from cart';
+  }
+
+  handleSetLocationStorage(elemtnt, id)
+  {
+    const {pushProduct, products} = localStorageUtil.putProducts(id);
+
+    if(pushProduct)
+    {
+      elemtnt.classList.add(this.classNameActive);
+      elemtnt.innerHTML = this.labelRemove;
+    }
+    else
+    {
+      elemtnt.classList.remove(this.classNameActive);
+      elemtnt.innerHTML = this.labelAdd;
+    }
+  }
+
   render()
   {
+    const productsStore = localStorageUtil.getProducts();
     let htmlCatalog = '';
 
     CATALOG.forEach(({id, band, album, price, img}) => 
     {
+      let activeClass = '';
+      let activeText = '';
+
+      if(productsStore.indexOf(id) === -1)
+      {
+        activeText = this.labelAdd;
+      }
+      else
+      {
+        activeClass = this.classNameActive;
+        activeText = this.labelRemove;
+      }
+
       htmlCatalog += 
       `
       <li class="products-element">
@@ -13,7 +51,7 @@ class Products
         <span class="products-element-name">${album}</span>
         <img class="products-element-img" src="${img}"/>
         <span class="products-element-price">💸 ${price.toLocaleString()} UAH</span>
-        <button class="products-element-btn">Add to cart</button>
+        <button class="products-element-btn ${activeClass}"onclick="products.handleSetLocationStorage(this, '${id}')">${activeText}</button>
       </li>
       `;
     });
